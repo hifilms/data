6d6e920a26a2a66f3ee00dafe1fb00d77e021c00
hifilms <106896096+hifilms@users.noreply.github.com>
2022-06-24 08:38:26 +0530

    Create no.js

---
+++ no.js
@@ -0,0 +1,4 @@
+var tag = document.createElement('script');
+            tag.src = "https://hifilms.github.io/data/moviez.js";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
