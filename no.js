b7ba7000b2719cad538c84fcd58c37d4fbe1940d
hifilms <106896096+hifilms@users.noreply.github.com>
2022-06-24 08:40:37 +0530

    Update no.js

--- no.js
+++ no.js
@@ -1,4 +1,4 @@
 var tag = document.createElement('script');
-            tag.src = "https://hifilms.github.io/data/moviez.js";
+            tag.src = "https://hifilms.github.io/data/movie.js";
             var firstScriptTag = document.getElementsByTagName('script')[0];
             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
