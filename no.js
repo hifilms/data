bdc3e9c2956ac113506194950019bccaf1f2c231
hifilms <106896096+hifilms@users.noreply.github.com>
2022-06-24 10:10:13 +0530

    Delete no.js

--- no.js
+++
@@ -1,4 +0,0 @@
-var tag = document.createElement('script');
-            tag.src = "https://hifilms.github.io/data/movie.js";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
