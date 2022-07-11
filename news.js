517661850ed39a2815811a26b6d21c7cbb3aa349
hifilms <106896096+hifilms@users.noreply.github.com>
2022-07-11 10:12:07 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,5 +1,6 @@
 var newsz = ["nyd-xznCpJc","H8U3UwRvTBs","GE_SfNVNyqk","Xmm3Kr5P1Uw","IuDb_Aopq7I","QmERLqjlHdo","EguYDSbSBZM","KyUT37gNm5w","qfrocHBy6RQ","TD0A7fHAxKw","MN8p-Vrn6G0","JiVA-dQ6dsc","wBhSCUEMbg4","e3JMsqUEwWk","9Auq9mYxFEE","WB-y7_ymPJ4","h3MuIUNCCzI","TdQShwOfWSA"]
 var news =[
   ["nyd-xznCpJc","abp","",""],
-  ["nyd-xznCpJc","abp","",""],
+  ["H8U3UwRvTBs","news18","",""],
+  ["GE_SfNVNyqk","wd","",""],
 ]
