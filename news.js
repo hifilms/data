8603dc35f687538bc47eef52b207007cfaf437ca
hifilms <106896096+hifilms@users.noreply.github.com>
2022-07-11 09:40:03 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,1 +1,5 @@
-var news = ["nyd-xznCpJc","H8U3UwRvTBs","GE_SfNVNyqk","Xmm3Kr5P1Uw","IuDb_Aopq7I","QmERLqjlHdo","EguYDSbSBZM","KyUT37gNm5w","qfrocHBy6RQ","TD0A7fHAxKw","MN8p-Vrn6G0","JiVA-dQ6dsc","wBhSCUEMbg4","e3JMsqUEwWk","9Auq9mYxFEE","WB-y7_ymPJ4","h3MuIUNCCzI","TdQShwOfWSA"]
+var newsz = ["nyd-xznCpJc","H8U3UwRvTBs","GE_SfNVNyqk","Xmm3Kr5P1Uw","IuDb_Aopq7I","QmERLqjlHdo","EguYDSbSBZM","KyUT37gNm5w","qfrocHBy6RQ","TD0A7fHAxKw","MN8p-Vrn6G0","JiVA-dQ6dsc","wBhSCUEMbg4","e3JMsqUEwWk","9Auq9mYxFEE","WB-y7_ymPJ4","h3MuIUNCCzI","TdQShwOfWSA"]
+var news =[
+  ["nyd-xznCpJc","abp","",""],
+  ["nyd-xznCpJc","abp","",""],
+]
