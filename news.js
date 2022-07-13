39d002a3045fc528e725b650212f09028452c4d6
hifilms <106896096+hifilms@users.noreply.github.com>
2022-07-13 13:47:57 +0530

    Update news.js

--- news.js
+++ news.js
@@ -12,7 +12,7 @@ var news =[
   ["MN8p-Vrn6G0","ndtvIndia","",""],
   ["-aNLNkmqtsg","timesnow","",""],
   ["wBhSCUEMbg4","zeenews","",""],
-  ["aC650xB9NNc","tv9bharatvarsh","",""],
+  ["aC650xB9NNc","news24","",""],
   ["9Auq9mYxFEE","skynews","",""],
   ["WB-y7_ymPJ4","ndtv","",""],
   ["h3MuIUNCCzI","France24","",""],
