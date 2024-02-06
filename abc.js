691ca4d07c1016f02e35546641843e3b7e58ce28
hifilms <106896096+hifilms@users.noreply.github.com>
2024-02-06 14:33:04 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,39 @@
+		var passwordx = "AS0013";
+		var headerx = "অগ্রদূত সমিতি";
+		var footerx = "Developed by b roy (v.1.6)";
+		var h1 = "<h1>সুশান্তর ৫০০ বাকি?</h1>"; //
+		
+		var vx = [//ADMIN
+			["01) Chotan Halder", 5500],
+			["02) Biswabyapi Halder", 5500],
+			["03) Suman Biswas", 5000],
+			["04) Prosen Halder", 5500],
+			["05) Amit Halder", 5500],
+			["06) Bikram Halder", 5500],
+			["07) Bappa Mondal", 5500],
+			["08) Ramprasad Halder", 5500],
+			["09) Subrata Mondal", 5500],
+			["10) Upanada Halder", 5000],
+			["11) Pravat Biswas", 5500],
+			["12) Rajib Mondal", 5500],
+			["13) Sourav Halder", 5500]];
+
+		var vy = [ //EXPENSES
+			["Salary", 200]];
+
+		var ary = [ //1.Name, 2.date, 3.amount, 4.charge, 5.interest, 6.return, 7.fine, 8.installment
+			["AS0009 - Amio Sarkar","04.02.2024",5000,50,0,0,0,"13"],
+          	["AS0008 - Sudip Sarkar","30.01.2024",3000,30,0,150,0,"25"],
+			["AS0007 - Bapi Pramanik","24.01.2024",10000,100,0,1000,0,"24"],
+			["AS0006 - Sushanta Halder","21.01.2024",10000,100,0,500,0,"25"],
+			["AS0005 - Bipul Halder","09.01.2024",20000,200,0,0,0,"00"],
+			["AS0004 - Amio Sarkar","06.01.2024",3000,30,170,3000,0,"Paid"],
+			["AS0003 - Sima Pramanik","11.01.2024",10000,100,0,2000,0,"22"],
+			["AS0002 - Sujoy Halder","05.01.2024",10000,100,0,2500,0,"21"],
+			["AS0001 - Haradhan Halder","03.01.2024",10000,100,0,2500,0,"21"]];
+
+
+
+
+
+
