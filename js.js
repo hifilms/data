6398e9f882f13205b2a04f2e50e70462fcf3ae59
hifilms <106896096+hifilms@users.noreply.github.com>
2024-02-19 09:20:44 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 		var passwordx = "AS0013";
 		var headerx = "অগ্রদূত সমিতি";
-		var footerx = "Last updated (18.02.2024)";
-		var h1 = "<h1>সুশান্তর ৫০০ বাকি?</h1>"; //
+		var footerx = "Last updated (19.02.2024)";
+		var h1 = ""; //<h1>সুশান্তর ৫০০ বাকি?</h1>
 		
 		var vx = [//ADMIN
 			["01) Chotan Halder", 5500],
@@ -24,9 +24,9 @@
 		var ary = [ //1.Name, 2.date, 3.amount, 4.charge, 5.interest, 6.return, 7.fine, 8.installment
 			["AS0010 - Ajay swarnakar","07.02.2024",3000,30,0,300,0,"24"],
 			["AS0009 - Amio Sarkar","04.02.2024",5000,50,0,900,0,"11"],    		
-   ["AS0008 - Sudip Sarkar","30.01.2024",3000,30,0,450,0,"23"],
+   			["AS0008 - Sudip Sarkar","30.01.2024",3000,30,0,450,0,"23"],
 			["AS0007 - Bapi Pramanik","24.01.2024",10000,100,0,2000,0,"22"],
-			["AS0006 - Sushanta Halder","21.01.2024",10000,100,0,1500,200,"23"],
+			["AS0006 - Sushanta Halder","21.01.2024",10000,100,0,2000,200,"22"],
 			["AS0005 - Bipul Halder","09.01.2024",20000,200,1200,0,0,"00"],
 			["AS0004 - Amio Sarkar","06.01.2024",3000,30,170,3000,0,"Paid"],
 			["AS0003 - Sima Pramanik","11.01.2024",10000,100,0,3000,0,"20"],
