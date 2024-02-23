0935c74ea292936184b04a394b66fcfa3b125e27
hifilms <106896096+hifilms@users.noreply.github.com>
2024-02-23 12:02:24 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 		var passwordx = "AS0013";
 		var headerx = "অগ্রদূত সমিতি";
-		var footerx = "Last updated (19.02.2024)";
+		var footerx = "Last updated (23.02.2024)";
 		var h1 = ""; //<h1>সুশান্তর ৫০০ বাকি?</h1>
 		
 		var vx = [//ADMIN
@@ -19,13 +19,13 @@
 			["13) Sourav Halder", 5500]];
 
 		var vy = [ //EXPENSES
-			["Salary", 200]];
+			["Salary - (1)", 200]];
 
 		var ary = [ //1.Name, 2.date, 3.amount, 4.charge, 5.interest, 6.return, 7.fine, 8.installment
 			["AS0011 - Ananda Swarnakar","19.02.2024",6000,60,0,0,0,"26"],
 			["AS0010 - Ajay Swarnakar","07.02.2024",3000,30,0,300,0,"24"],
 			["AS0009 - Amio Sarkar","04.02.2024",5000,50,0,900,0,"11"],    		
-   			["AS0008 - Sudip Sarkar","30.01.2024",3000,30,0,450,0,"23"],
+   			["AS0008 - Sudip Sarkar","30.01.2024",3000,30,0,1500,0,"16"],
 			["AS0007 - Bapi Pramanik","24.01.2024",10000,100,0,2000,0,"22"],
 			["AS0006 - Sushanta Halder","21.01.2024",10000,100,0,2000,200,"22"],
 			["AS0005 - Bipul Halder","09.01.2024",20000,200,1200,0,0,"00"],
