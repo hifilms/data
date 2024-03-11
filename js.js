2c58d9bf0e8507e687da4557d904d3f2339788b8
hifilms <106896096+hifilms@users.noreply.github.com>
2024-03-11 21:52:57 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 		var passwordx = "AS0013";
 		var headerx = "অগ্রদূত সমিতি";
-		var footerx = "Last updated (07.03.2024)";
-		var h1 = ""; //<h1>সীমা ও হারাধনের ১ কিস্তি বাকি।</h1>
+		var footerx = "Last updated (11.03.2024)";
+		var h1 = "<h1>Ananda Swarnakar ১ কিস্তি বাকি।</h1>"; //
 		
 		var vx = [//ADMIN
 			["01) Chotan Halder", 6000],
@@ -14,27 +14,27 @@
 			["08) Ramprasad Halder", 6000],
 			["09) Subrata Mondal", 6000],
 			["10) Upanada Halder", 5500],
-			["11) Pravat Biswas", 5500],
+			["11) Pravat Biswas", 6000],
 			["12) Rajib Mondal", 6000],
-			["13) Sourav Halder", 5500]];
+			["13) Sourav Halder", 6000]];
 
 		var vy = [ //EXPENSES
 			["Salary - (February)", 200],
 			["Salary - (January)", 200]];
 
 		var ary = [ //1.Name, 2.date, 3.amount, 4.charge, 5.interest, 6.return, 7.fine, 8.installment
-			["AS0012 - Subrata Halder","26.02.2024",10000,100,0,500,0,"25"],
+			["AS0012 - Subrata Halder","26.02.2024",10000,100,0,1000,0,"24"],
 			["AS0011 - Ananda Swarnakar","19.02.2024",6000,60,0,600,0,"24"],
-			["AS0010 - Ajay Swarnakar","07.02.2024",3000,30,0,600,0,"22"],
-			["AS0009 - Amio Sarkar","04.02.2024",5000,50,0,1850,0,"09"],    		
+			["AS0010 - Ajay Swarnakar","07.02.2024",3000,30,0,750,0,"21"],
+			["AS0009 - Amio Sarkar","04.02.2024",5000,50,0,2250,50,"08"],    		
    			["AS0008 - Sudip Sarkar","30.01.2024",3000,30,0,1500,0,"16"],
-			["AS0007 - Bapi Pramanik","24.01.2024",10000,100,0,3000,0,"20"],
-			["AS0006 - Sushanta Halder","21.01.2024",10000,100,0,3000,200,"20"],
-			["AS0005 - Bipul Halder","09.01.2024",20000,200,1200,0,0,"00"],
+			["AS0007 - Bapi Pramanik","24.01.2024",10000,100,0,3500,0,"19"],
+			["AS0006 - Sushanta Halder","21.01.2024",10000,100,0,3500,200,"19"],
+			["AS0005 - Bipul Halder","09.01.2024",20000,200,2400,0,0,"00"],
 			["AS0004 - Amio Sarkar","06.01.2024",3000,30,170,3000,0,"Paid"],
 			["AS0003 - Sima Pramanik","11.01.2024",10000,100,0,4500,0,"17"],
-			["AS0002 - Sujoy Halder","05.01.2024",10000,100,0,4500,50,"17"],
-			["AS0001 - Haradhan Halder","03.01.2024",10000,100,0,4500,0,"17"]];
+			["AS0002 - Sujoy Halder","05.01.2024",10000,100,0,5000,50,"16"],
+			["AS0001 - Haradhan Halder","03.01.2024",10000,100,0,5000,0,"16"]];
 
 
 
